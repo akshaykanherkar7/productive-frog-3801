2034a6d4e2c44a6381e8c5586976253f986729b8
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-08-25 22:40:33 +0530

    day3

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,14 +3,14 @@ import MainRoutes from "./Pages/MainRoutes";
 import Noinvoicebox from "./Components/Bharat/Noinvoicebox"
 import Invoiceedit from "./Components/Bharat/Invoiceedit ";
 import Trialbox from "./Components/Bharat/trialbox";
+import Invoicestable from "./Components/Bharat/Invoicestable";
 
 function App() {
   return (
     <div>
-      <Trialbox/>
       {/* <MainRoutes></MainRoutes> */}
-      <Noinvoicebox/>
-      {/* <Invoiceedit/> */}
+
+      <Invoicestable/>
     </div>
   );
 }

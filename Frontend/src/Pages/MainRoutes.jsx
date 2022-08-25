b8ee311a4c1d5b3efaec876823a44f55ab89f76d
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-08-25 09:18:08 +0530

    Added ICON Depe

--- Frontend/src/Pages/MainRoutes.jsx
+++ Frontend/src/Pages/MainRoutes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import SignUp from "./SignUp";
+import Homepage1 from "./Homepage1"
 
 const MainRoutes = () => {
   return (

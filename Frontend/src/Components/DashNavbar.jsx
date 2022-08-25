f21412809774a7c336575f77e99f0329a33038e1
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-08-25 19:24:50 +0530

    dashboard

--- Frontend/src/Components/DashNavbar.jsx
+++ Frontend/src/Components/DashNavbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export const DashNavbar = () => {
   return (
     <Box>
-        
+        <Box boxShadow="md" rounded="md" bg="white">
+          
+        </Box>
     </Box>
   )
 }

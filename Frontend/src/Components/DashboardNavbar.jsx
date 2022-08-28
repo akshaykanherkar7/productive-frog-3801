2d666ea8466e98d3478b20ad1ddc71b099e06050
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-08-28 15:36:07 +0530

    Done

--- Frontend/src/Components/DashboardNavbar.jsx
+++ Frontend/src/Components/DashboardNavbar.jsx
@@ -1,8 +1,32 @@
 import { ChevronDownIcon, SearchIcon, TimeIcon } from "@chakra-ui/icons";
-import { Box, Button, Flex, Input, Text, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Input,
+  Text,
+  Image,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+} from "@chakra-ui/react";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logoutAPI } from "../Redux/Auth/auth.action";
 
 const DashboardNavbar = () => {
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  let username = userData.fullname.split(" ")[0];
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logoutAPI());
+    navigate("/");
+  };
   return (
     <Box border="1px solid gray" borderLeft={"none"} w="100%" h="7%">
       <Flex justifyContent="space-between">
@@ -28,10 +52,10 @@ const DashboardNavbar = () => {
           // border="1px solid red"
           h="50"
           mt="2"
-          w="35%"
+          w="45%"
         >
           <Flex justifyContent="space-evenly">
-            <Button mt="1.5" h="8" colorScheme="teal" size="md">
+            <Button mt="1.5" h="8" colorScheme="teal" size="md" ml="50">
               Start Free Trial
             </Button>
             <TimeIcon
@@ -41,21 +65,53 @@ const DashboardNavbar = () => {
               h={8}
               position="absolute"
             ></TimeIcon>
-            <Text mt="2" ml="10" cursor={"pointer"}>
+            <Text mt="2" ml="7" cursor={"pointer"}>
               Start Timer
             </Text>
-            <Box w="12%">
-              <Flex justifyContent="space-between">
-                <Image
-                  id="mh"
-                  cursor={"pointer"}
-                  h="30"
-                  w="30"
-                  mt="2"
-                  src="https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688"
-                ></Image>
-                <ChevronDownIcon cursor={"pointer"} mt="4"></ChevronDownIcon>
-              </Flex>
+            <Box w="12%" mt="2" marginLeft="8">
+              <Menu>
+                <Flex
+                  marginLeft="-50px"
+                  display="flex"
+                  gap="10px"
+                  alignItems="center"
+                >
+                  <Box>
+                    <i class="fa-solid fa-user"></i>
+                  </Box>
+                  <MenuButton>
+                    {username}
+                    <i class="fa-solid fa-angle-down"></i>
+                  </MenuButton>
+                </Flex>
+                <MenuList marginLeft="35%" mt="2">
+                  <MenuItem minH="48px" gap="30px">
+                    <span>Get Bonsai Free</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>Help Center</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>What's New</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>My Subscription</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>Apps & Integrations</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>Payments</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span>Setting</span>
+                  </MenuItem>
+                  <MenuItem minH="40px" gap="30px">
+                    <span onClick={handleLogout}>Logout</span>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              {/* </Flex> */}
             </Box>
           </Flex>
         </Box>

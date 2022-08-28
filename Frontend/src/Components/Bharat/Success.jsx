cc5afa569ee5b409fe34e21061a579e412ddd2a7
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-08-28 15:15:16 +0530

    m

--- Frontend/src/Components/Bharat/Success.jsx
+++ Frontend/src/Components/Bharat/Success.jsx
@@ -1,27 +1,30 @@
 import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react'
 import React from 'react'
+import success from "./success.module.css"
+import {useNavigate} from "react-router-dom"
 
 const Success = () => {
       const { isOpen, onOpen, onClose } = useDisclosure()
+      const navigate=useNavigate ()
+      const handleclose=()=>{
+        navigate("/dashboard")
+      }
   return (
     <div>
          <Button onClick={onOpen}>Open Modal</Button>
 
-      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={ onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+        <ModalContent padding={"50px 50px 50px 50px"}>
           <ModalCloseButton />
-          <ModalBody>
-            <Text fontWeight='bold' mb='1rem'>
-              You can scroll the content behind the modal
-            </Text>
-            {/* <Lorem count={2} /> */}
+          <ModalBody >
+               <img src='success.svg'/>
+               <Text className={success.text}>Invice Created successfully !</Text>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={onClose}>
-              Close
+            <Button colorScheme='blue' margin={"auto"} marginTop="20px" onClick={ onClose}>
+              Go to Dashboard
             </Button>
           </ModalFooter>
         </ModalContent>
